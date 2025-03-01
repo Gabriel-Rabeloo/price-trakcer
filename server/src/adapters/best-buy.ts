a3f4d3b7a3142b28a900d9adb473a4d0a9872e19
Gabriel Rabelo <gabriiel.rabeloo@gmail.com>
2025-02-28 23:45:54 -0300

    fix: removed timeout on puppeteer

--- server/src/adapters/best-buy.ts
+++ server/src/adapters/best-buy.ts
@@ -3,7 +3,7 @@ import { pricerNormalizer } from '../utils';
 
 export class BestBuy {
     static async getPrice({ url, page }: { url: string; page: Page }): Promise<number | null> {
-        await page.goto(url, { waitUntil: ['load', 'domcontentloaded', 'networkidle0', 'networkidle2'] });
+        await page.goto(url, { waitUntil: ['load', 'domcontentloaded', 'networkidle0', 'networkidle2'], timeout: 0 });
         await page.setViewport({ width: 1920, height: 1080 });
         const price = await page.evaluate(async () => {
             if (!document.getElementsByClassName('style-module_price__ql4Q1 style-module_mediumLarge__QtUDJ')[0]?.childNodes?.[0]?.textContent) {
