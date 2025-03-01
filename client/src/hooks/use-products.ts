05cad7c9da1e4390f706cd0880e1d12bd4d79075
Gabriel Rabelo <gabriiel.rabeloo@gmail.com>
2025-02-28 22:20:53 -0300

    chore: new deploy at vercel

--- client/src/hooks/use-products.ts
+++ client/src/hooks/use-products.ts
@@ -11,6 +11,6 @@ export const useProducts = () => {
     return useQuery({
         queryKey: ['products'],
         queryFn: fetchProducts,
-        staleTime: 1000 * 60 * 5, // Cache for 5 minutes
+        staleTime: 1000 * 60 * 5, //  Cache for 5 minutes
     });
 };
