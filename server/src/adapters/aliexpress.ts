eb448515d2873c83202b7957106028dd222b043d
Gabriel Rabelo <gabriiel.rabeloo@gmail.com>
2025-03-01 14:32:33 -0300

    feat: improved retries to get price and changed favicon

--- server/src/adapters/aliexpress.ts
+++ server/src/adapters/aliexpress.ts
@@ -5,8 +5,14 @@ import { GetPriceAdapter } from './interface';
 export class Aliexpress implements GetPriceAdapter {
     async getPrice({ page }: { page: Page }): Promise<number | null> {
         const price = await page.evaluate(async () => {
-            if (!document.getElementsByClassName('price--currentPriceText--V8_y_b5 pdp-comp-price-current product-price-value')?.[0]?.textContent) {
-                await new Promise((r) => setTimeout(r, 100000));
+            let tries = 0;
+
+            while (
+                !document.getElementsByClassName('price--currentPriceText--V8_y_b5 pdp-comp-price-current product-price-value')?.[0]?.textContent &&
+                tries < 200
+            ) {
+                await new Promise((r) => setTimeout(r, 1000));
+                tries++;
             }
 
             return document.getElementsByClassName('price--currentPriceText--V8_y_b5 pdp-comp-price-current product-price-value')?.[0]?.textContent;
--- server/src/adapters/best-buy.ts
+++ server/src/adapters/best-buy.ts
@@ -1,11 +1,18 @@
 import { Page } from 'puppeteer';
 import { pricerNormalizer } from '../utils';
+import { GetPriceAdapter } from './interface';
 
-export class BestBuy {
+export class BestBuy implements GetPriceAdapter {
     async getPrice({ page }: { page: Page }): Promise<number | null> {
         const price = await page.evaluate(async () => {
-            if (!document.getElementsByClassName('style-module_price__ql4Q1 style-module_mediumLarge__QtUDJ')[0]?.childNodes?.[0]?.textContent) {
-                await new Promise((r) => setTimeout(r, 100000));
+            let tries = 0;
+
+            while (
+                !document.getElementsByClassName('price--currentPriceText--V8_y_b5 pdp-comp-price-current product-price-value')?.[0]?.textContent &&
+                tries < 200
+            ) {
+                await new Promise((r) => setTimeout(r, 1000));
+                tries++;
             }
 
             return document.getElementsByClassName('style-module_price__ql4Q1 style-module_mediumLarge__QtUDJ')[0]?.childNodes?.[0]?.textContent;
